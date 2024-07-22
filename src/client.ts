31f51a24a9dea4cda0d35d61d799a15bca057011
Marlom Girardi <marlom@rvvup.com>
2024-07-22 11:43:08 +0100

    extract base url from token

--- src/client.ts
+++ src/client.ts
@@ -37,7 +37,7 @@ export class RvvupClient {
 
   constructor(authToken: string, options: RvvupClientOptions = {}) {
     const jwt = jwtDecode<RvvupJwtPayload>(authToken);
-    const baseUrl = options.baseUrl ?? jwt.aud;
+    const baseUrl = options.baseUrl ?? typeof jwt.aud !== "string" ? null : jwt.aud.replace("/graphql", "");
     const merchantId = options.merchantId ?? jwt.merchantId;
 
     if (typeof baseUrl !== "string") {
